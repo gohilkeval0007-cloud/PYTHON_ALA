edb6f5948e0ed158b3a7811d78a2a13fbecad8fc
gohilkeval0007-cloud <gohilkeval0007@gmail.com>
2026-02-20 10:29:50 +0530

    Update Error.py

--- ALA1/Error.py
+++ ALA1/Error.py
@@ -1,1 +1,14 @@
+print("Exam Registration")
+students = []
+for i in range(3)   # Error: Missing colon :
+ name = input("Enter name: ")
+ students.append(name)
+print("Registered:", student)   # Error: Wrong variable name
+if len(students) > 0   # Error: Missing colon :
+ print("Registration Success")
+else   # Error: Missing colon :
+ print("No Students")
+for i in range(2)   # Error: Missing colon :
+print("Done")   # Error: No indentation
+print("End")
 
