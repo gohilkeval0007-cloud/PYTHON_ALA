c52994c43a4142af6df9a0b1e7c35f1c812fe46c
gohilkeval0007-cloud <gohilkeval0007@gmail.com>
2026-02-20 10:28:12 +0530

    Update Solution.py

--- ALA1/Solution.py
+++ ALA1/Solution.py
@@ -1,1 +1,19 @@
+print("Exam Registration")
 
+students = []
+
+for i in range(3):   # ❗ colon added
+    name = input("Enter name: ")
+    students.append(name)
+
+print("Registered:", students)   # ❗ fixed variable name
+
+if len(students) > 0:   # ❗ colon added
+    print("Registration Success")
+else:   # ❗ colon added
+    print("No Students")
+
+for i in range(2):   # ❗ colon added
+    print("Done")
+
+print("End")
